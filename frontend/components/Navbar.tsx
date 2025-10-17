0dda58c21b7552a3c90e7cefbe2682cb29aa4579
Joeyyyys <josephgiler2@gmail.com>
2025-10-17 12:43:37 -0400

    added a REUSABLE nav bar component using raw css and react-next, with clickable HREF links for our 3 main pages

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,36 @@
+'use client'; 
+
+import Link from 'next/link';
+import React from 'react';
+import styles from './Navbar.module.css'; 
+
+const navLinks = [
+  { name: 'Home', href: '/' },
+  { name: 'Map', href: '/about' },
+  { name: 'Contact', href: '/contact' },
+];
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">
+          WeatherOrNot Logo
+        </Link>
+      </div>
+
+      {/* Navigation Links */}
+      <ul className={styles.navLinks}>
+        {navLinks.map((link) => (
+          <li key={link.name}>
+            <Link href={link.href}>
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
